f824a63746400224d6dbc57e781b292b7422dbf8
Nyelus101 <cmuonyelu@gmail.com>
2024-08-22 20:22:24 +0100

    update

--- rest-countries-api-project/src/components/Header.jsx
+++ rest-countries-api-project/src/components/Header.jsx
@@ -23,6 +23,30 @@
 
 
 // src/components/Header.js
+// import React from 'react';
+// import { useTheme } from '../components/ThemeContext';
+
+// const Header = () => {
+//   const { darkMode, toggleDarkMode } = useTheme();
+
+//   return (
+//     <header className="p-4 bg-white dark:bg-gray-800 text-black dark:text-white flex justify-between items-center">
+//       <h1 className="text-xl font-bold">Where in the World?</h1>
+//       <button 
+//         onClick={toggleDarkMode} 
+//         className="p-2 rounded-full border focus:outline-none"
+//       >
+//         {darkMode ? 'Light Mode' : 'Dark Mode'}
+//       </button>
+//     </header>
+//   );
+// };
+
+// export default Header;
+
+
+
+
 import React from 'react';
 import { useTheme } from '../components/ThemeContext';
 
@@ -30,7 +54,7 @@ const Header = () => {
   const { darkMode, toggleDarkMode } = useTheme();
 
   return (
-    <header className="p-4 bg-white dark:bg-gray-800 text-black dark:text-white flex justify-between items-center">
+    <header className="py-4 px-[5%]   text-black  flex justify-between items-center shadow-gray-500">
       <h1 className="text-xl font-bold">Where in the World?</h1>
       <button 
         onClick={toggleDarkMode} 
