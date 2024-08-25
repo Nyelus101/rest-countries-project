261333b5620cb8aa3a9a266a5df5ab0b9eb49dfe
Nyelus101 <cmuonyelu@gmail.com>
2024-08-25 09:19:29 +0100

    update

--- rest-countries-api-project/src/components/Layout.jsx
+++ rest-countries-api-project/src/components/Layout.jsx
@@ -14,18 +14,3 @@ export default Layout;
 
 
 
-
-// // src/components/Layout.js
-// import React from 'react';
-// import Header from './Header';
-
-// const Layout = ({ children }) => {
-//   return (
-//     <div className="min-h-screen bg-white dark:bg-gray-900 text-black dark:text-white">
-//       <Header />
-//       {children}
-//     </div>
-//   );
-// };
-
-// export default Layout;
